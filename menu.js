e63be53bded40337fbbadd69b808207c3370ce2a
Shamuwel Ansari <shamuwelansari@gmail.com>
2023-06-28 16:40:15 +0530

    Main menu created.

---
+++ menu.js
@@ -0,0 +1,50 @@
+const readline = require('readline')
+
+
+let accountList = ["Account1", "Account2"];
+
+let menuList = [...accountList, "Add Account", "Exit"]
+
+console.log(accountList)
+
+console.log(menuList)
+
+console.log("Select an account or add new account : ")
+
+for(let i = 0; i < menuList.length; i++ ){
+    console.log(`${i + 1}. ${menuList[i]}`)
+}
+
+const r1 = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+r1.question("Enter an option", (userInput) => {
+    userInput = parseInt(userInput) - 1;
+    console.log(menuList[userInput])
+    console.log(`${menuList.length}`)
+
+    switch (userInput){
+        case menuList.length - 1:
+            console.log("Exiting....");
+            break;
+        case menuList.length - 2:
+            console.log("Adding Account..")
+            break;
+
+        default:
+            if(userInput >= 0 && userInput <= menuList.length - 3) {
+                console.log(` Switching account to account ${accountList[userInput]}`);
+                break;
+            }
+            else{
+                console.log("Invalid input");
+                break;
+            }
+
+    }
+
+
+    r1.close();
+});
