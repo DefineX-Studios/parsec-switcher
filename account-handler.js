b313c984fb2f2282e6b77b6ae7b1e55f235dad28
Shamuwel Ansari <shamuwelansari@gmail.com>
2023-07-10 18:56:39 +0530

    Developed addaccount function

--- account-handler.js
+++ account-handler.js
@@ -1,14 +1,60 @@
 let accountList = [];
-
-
-accountList.push("Account1");
-accountList.push("Account2");
-accountList.push("Account3");
-
-function addAccount(nickname){
-    accountList.push(nickname);
-    console.log(accountList)
+const {findProcess,stopProcess,startProcess} = require('./process-handler')
+const {removeParsecUserFile,createAccount,checkNickname} = require('./account-data')
+const sleep = require('sync-sleep')
+class ProcessException extends Error{
+    constructor(message) {
+        super(message);
+        this.name = 'ProcessException';
+    }
 }
+
+async function addAccount(nickname){
+    try {
+        let checkNameExists = checkNickname(nickname)
+        console.log("Adding account..")
+        //check if parsec is running
+        const output = await findProcess('parsecd')
+        if(output.success){
+            const stop = await stopProcess()
+            if (!stop.success){
+                throw new Error(stop.message)
+            }
+        }
+        while(true){
+
+            const findAgain = await findProcess('parsecd')
+            if (findAgain.success){
+                sleep.sleep(1000,()=>{})
+
+
+            }
+            else {
+                break
+            }
+
+        }
+        removeParsecUserFile()
+        console.log("Add account and exit parsec")
+
+        startProcess().then((output=>{
+            if(!output.success){
+                throw new ProcessException(output.message)
+            }
+            createAccount(nickname)
+
+
+        }))
+
+
+    }
+    catch (error){
+        console.log(error.message)
+    }
+
+}
+
+
 
 function deleteAccount(nickname){
     let listWithoutNickname = [];
@@ -30,6 +76,9 @@ function returnAccountList(){
     return accountList;
 
 }
+
+
+//addAccount('mute')
 
 module.exports = {
     addAccount,
