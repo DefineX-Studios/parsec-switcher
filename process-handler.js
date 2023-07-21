118277f2e34e3af5068cb654edf0d31c9ddf424c
Shamuwel Ansari <shamuwelansari@gmail.com>
2023-07-21 16:59:48 +0530

    Fixes #8

--- process-handler.js
+++ process-handler.js
@@ -29,8 +29,9 @@ function stopProcess(){
  */
     let stopOutput = {}
     return new Promise((resolve)=>{
-        console.log("inside stop")
-        exec('tools\\stop_process.bat',(error,stdout,stderr)=>{
+        //console.log("inside stop")
+        let stopLocation = path.join(__dirname,'tools','stop_process.bat')
+        exec(stopLocation,(error,stdout,stderr)=>{
             if(error){
                 stopOutput.success = 0;
                 stopOutput.message = error;
@@ -51,13 +52,13 @@ function stopProcess(){
 }
 
 function startProcess(){
-    console.log("Inside start")
+    //console.log("Inside start")
     //let batAndLocation = 'tools\\start_process.bat "C:\\Program Files\\Parsec\\parsecd.exe"'  //Include this in config later
     const appDataPath = path.join(os.homedir(), 'AppData', 'Roaming', 'parsec-switcher');
     let configFilePath = path.join(appDataPath,'config.json')
     let file = fs.readFileSync(configFilePath,'utf-8')
     let configFile = JSON.parse(file)
-    console.log("Trying to open JSON")
+    //console.log("Trying to open JSON")
     let batAndLocation = configFile['parsecdLocation']
 
     const options = {
@@ -94,8 +95,9 @@ function startProcess(){
 }
 
 function findProcess(process){
-    console.log("inside find")
-    let batAndLocation = `./tools/find_process.ps1 ${process}`;
+    //console.log("inside find")
+    let scriptLocation = path.join(__dirname,'tools','find_process.ps1')
+    let batAndLocation = `${scriptLocation} ${process}`;
     /*
     let commandOutput = {};
     //let batAndLocation = `get-process parsecd`;
@@ -124,8 +126,35 @@ function findProcess(process){
     //console.log(batAndLocation)
     let commandOutput = {}
     return new Promise((resolve)=>{
+
+        /*
         execFile(batAndLocation,{'shell': 'powershell.exe'},(error,stdout,stderr)=>{
             console.log("Inside exec find")
+            //console.log("In find process exec")
+            //console.log(`error : ${error}, stdout : ${stdout}, stderr : ${stderr}`)
+
+            if(error){
+                commandOutput.success = 0;
+                commandOutput.message = error.message;
+                resolve(commandOutput)
+                return;
+            }
+            if (stderr){
+                //console.log("Got in stderr")
+                commandOutput.success = 0;
+                commandOutput.message = stderr;
+                //console.log(commandOutput)
+                resolve(commandOutput);
+                return;
+            }
+            commandOutput.success = 1;
+            commandOutput.message = stdout;
+            resolve(commandOutput)
+        })
+
+         */
+        exec(batAndLocation,{'shell': 'powershell.exe'},(error,stdout,stderr)=>{
+            //console.log("Inside exec find")
             //console.log("In find process exec")
             //console.log(`error : ${error}, stdout : ${stdout}, stderr : ${stderr}`)
 
