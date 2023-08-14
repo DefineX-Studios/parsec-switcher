f947a6a730ed0b1c06ab9880fc56b0cb033e666f
Shamuwel Ansari <shamuwelansari@gmail.com>
2023-08-14 18:40:48 +0530

    Changes to how parsecd is not found handled

--- cli.js
+++ cli.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
-//console.log("0")
 const {addAccount,deleteAccount,returnAccountList, switchAccount} = require('./lib/account-handler')
-//console.log("1")
 const {global_state,initialize} = require("./lib/initialize")
-//console.log("2")
 
-//cnsole.log("3")
 const docopt = require('docopt').docopt;
 
 //console.log(4)
@@ -34,43 +30,37 @@ Options:
 
 const options = docopt(doc, { version: '0.0.1' });
 
-try {
-    if(!global_state.config["parsecdFound"]){
-        console.log("Parsecd not found, run \"parsec-switcher setup <parsecdLocation>\" ")
+if(!global_state.config["parsecdFound"]){
+    console.log("Parsecd not found, run \"parsec-switcher setup <parsecdLocation>\" ")
+}
+else {
+    if (options['-a'] || options['--add']){
+
+        console.log(`Adding ${options['<nickname>']}`);
+        addAccount(options['<nickname>']);
+
     }
-    else {
-        if (options['-a'] || options['--add']){
-
-            console.log(`Adding ${options['<nickname>']}`);
-            global_state.onConfigChanged.push(rando)
-            addAccount(options['<nickname>']);
-
-        }
-        if (options['-d'] || options['--delete']){
-            console.log(` Deleting account ${options['<nickname>']}`);
-            deleteAccount(options['<nickname>']);
-        }
-        if (options['-s'] || options['--switch']){
-            console.log(`Switching account to ${options['<nickname>']}`);
-            switchAccount(options['<nickname>'])
-
-        }
-        if (options['-l'] || options['--list']){
-            console.log('Printing the list ')
-            list = returnAccountList()
-            console.log(list)
-        }
+    if (options['-d'] || options['--delete']){
+        console.log(` Deleting account ${options['<nickname>']}`);
+        deleteAccount(options['<nickname>']);
     }
+    if (options['-s'] || options['--switch']){
+        console.log(`Switching account to ${options['<nickname>']}`);
+        switchAccount(options['<nickname>'])
+
+    }
+    if (options['-l'] || options['--list']){
+        console.log('Printing the list ')
+        list = returnAccountList()
+        console.log(list)
+    }
+}
 
 
 
-}
-catch (error){
-    console.log(error.message)
-}
-function rando(){
-    console.log("add a new cat")
-}
+
+
+
 
 /*
 if (options['hello']) {
