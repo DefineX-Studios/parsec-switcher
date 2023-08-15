fc10ad1ef32a5a3d515f13655316795d8a99e253
Adam Saudagar <adsau59@gmail.com>
2023-08-15 13:46:03 +0530

    dont initialize if just starting gui

--- cli.js
+++ cli.js
@@ -4,9 +4,14 @@ const {global_state,initialize} = require("./lib/initialize")
 var child_process = require('child_process');
 const docopt = require('docopt').docopt;
 
-//console.log(4)
-
-initialize()
+function areAllValuesFalse(obj) {
+    for (const value of Object.values(obj)) {
+        if (value) {
+            return false;
+        }
+    }
+    return true;
+}
 
 
 const doc = `
@@ -28,33 +33,38 @@ Options:
   --version     Show version.
 `;
 
-const options = docopt(doc, { version: '0.0.1' });
+function main(){
+    const options = docopt(doc, { version: '0.0.1' });
 
-if(!global_state.config["parsecdFound"]){
-    console.log("Parsecd not found, run \"parsec-switcher setup <parsecdLocation>\" ")
-}
-else {
-    if (options['-a'] || options['--add']){
-
-        console.log(`Adding ${options['<nickname>']}`);
-        addAccount(options['<nickname>']);
-
-    }
-    else if (options['-d'] || options['--delete']){
-        console.log(` Deleting account ${options['<nickname>']}`);
-        deleteAccount(options['<nickname>']);
-    }
-    else if (options['-s'] || options['--switch']){
-        console.log(`Switching account to ${options['<nickname>']}`);
-        switchAccount(options['<nickname>'])
-
-    }
-    else if (options['-l'] || options['--list']){
-        console.log('Printing the list ')
-        list = returnAccountList()
-        console.log(list)
-    }
-    else{
+    if(areAllValuesFalse(options)){
         child_process.spawn(`${__dirname}\\node_modules\\.bin\\electron.cmd`, [`${__dirname}\\index.js`]);
+        return;
+    }
+
+    initialize();
+    if(!global_state.config["parsecdFound"]){
+        console.log("Parsecd not found, run \"parsec-switcher setup <parsecdLocation>\" ")
+    }
+    else {
+        if (options['-a'] || options['--add']){
+            console.log(`Adding ${options['<nickname>']}`);
+            addAccount(options['<nickname>']);
+        }
+        else if (options['-d'] || options['--delete']){
+            console.log(` Deleting account ${options['<nickname>']}`);
+            deleteAccount(options['<nickname>']);
+        }
+        else if (options['-s'] || options['--switch']){
+            console.log(`Switching account to ${options['<nickname>']}`);
+            switchAccount(options['<nickname>'])
+
+        }
+        else if (options['-l'] || options['--list']){
+            console.log('Printing the list ')
+            list = returnAccountList()
+            console.log(list)
+        }
     }
 }
+
+main();
