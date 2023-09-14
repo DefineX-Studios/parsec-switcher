14a998c4c1b436ee2cfe8d4b3bbdc167292246f0
Shamuwel Ahmed Ansai <shamuwelansari@gmail.com>
2023-09-14 18:23:45 +0530

    Removed the requirement for a seperate script file and integrated scripts into js

--- tools/check_admin.ps1
+++
@@ -1,9 +0,0 @@
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal(
-[Security.Principal.WindowsIdentity]::GetCurrent()
-)
-$isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-if ($isAdmin) {
-    Write-Host "Yes"
-} else {
-    Write-Host "No"
-}
--- tools/find_process.ps1
+++
@@ -1,3 +0,0 @@
-#tasklist
-$processName = $args[0]
-get-process $processName
