b52e502c05487ab5b968cf79f509e7996665354d
Shamuwel Ahmed Ansai <shamuwelansari@gmail.com>
2023-09-24 14:18:39 +0530

    Added beforequite routine when unload event happens, partly fixed #52, a new unelated bug was introduced when intialization tried to create the appData folder when electron main process already created that, so main is only run after  DOMContentLoaded event occurs.

--- gui/control.js
+++ gui/control.js
@@ -4,11 +4,17 @@ const {global_state} = require("../lib/initialize");
 const {initialize} = require("../lib/initialize");
 const {errorToMessage, error} = require("../lib/error");
 const {showToast, showYesNoPopup, showTextInputPopup} = require("./elements");
-
+const {beforeQuit} = require("../lib/before-quit")
+const {logger} = require("../lib/logger");
 
 
 const accountsContainer = document.getElementById('accounts-container');
 const addAccountButton = document.getElementById('add-account-btn');
+
+
+window.addEventListener('unload',  beforeQuit)
+window.addEventListener('DOMContentLoaded',main)
+
 
 async function addButtonPressed(){
     const nickname = await showTextInputPopup("Enter Nickname", "Add Account");
@@ -19,7 +25,7 @@ async function addButtonPressed(){
 }
 
 function render(){
-    console.log("rendering");
+    logger.debug("rendering");
 
     accountsContainer.innerHTML = "";
     const accountsDiv = document.createElement('div');
@@ -27,14 +33,14 @@ function render(){
     accountsContainer.appendChild(accountsDiv);
 
     const {accounts, currentUser} = PSS.returnAccountList();
-    console.log(JSON.stringify(accounts));
+    logger.debug(JSON.stringify(accounts));
     for (const nickname of accounts){
         const userCardString = templates.generate_user_card(nickname, nickname === currentUser);
         accountsDiv.insertAdjacentHTML('beforeend', userCardString);
 
         // Deleting User Profile
         document.getElementById(`switch-btn-${nickname}`).addEventListener('click',async function(){
-            console.log(`switching ${nickname}`)
+            logger.debug(`switching ${nickname}`)
             const error = await PSS.switchAccount(nickname);
             if(!error) return;
             showToast("Error!", errorToMessage[error])
@@ -43,7 +49,7 @@ function render(){
         document.getElementById(`delete-btn-${nickname}`).addEventListener('click',async function(){
             const agreed = await showYesNoPopup(`Are you sure you want to delete ${nickname} account?`, "Cancel", "Delete Account");
             if(!agreed) return;
-            console.log(`deleting ${nickname}`)
+            logger.debug(`deleting ${nickname}`)
             const error = await PSS.deleteAccount(nickname);
             if(!error) return;
             showToast("Error!", errorToMessage[error])
@@ -52,8 +58,9 @@ function render(){
 }
 
 async function main(){
-    // showToast(process.argv[1])
+
     const errorCode = await initialize();
+    logger.debug("initializing")
     if(errorCode) showToast("Error!", errorToMessage[errorCode]);
     if(!global_state.flags.parsecDataLocationFound) showToast("Error!", errorToMessage[error.PARSEC_NOT_INSTALLED])
     if(!global_state.flags.parsecdFound) showToast("Error!", error.PARSECD_NOT_IN_DEFAULT);
@@ -62,4 +69,3 @@ async function main(){
     addAccountButton.addEventListener('click', addButtonPressed);
     render();
 }
-main();
